ae3187a70567f0e28edf29da7dda7de85e7924c5
ZainabOmar <zainab.hammami.zh@gmail.com>
2019-01-16 11:54:10 +0200

    setting up navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { NavigateComponent } from './navigate/navigate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+  AppComponent,
+  NavigateComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  BrowserModule,
+  AppRoutingModule,
+  ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+  BrowserAnimationsModule,
+  LayoutModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
